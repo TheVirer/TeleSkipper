270af38b429679f3161e7beb27956ef100246d97
TheVirer <sanekminin9@gmail.com>
2024-10-19 17:07:05 +0400

    first commit

--- Bot/Keyboards/profile_keyboards.py
+++ Bot/Keyboards/profile_keyboards.py
@@ -2,5 +2,3 @@ from aiogram.types import InlineKeyboardMarkup,InlineKeyboardButton
 
 profile_kb = InlineKeyboardMarkup(row_width=1)
 profile_button1 = InlineKeyboardButton(text="💎 Мои подписки", callback_data="my_subscriptions")
-profile_button2 = InlineKeyboardButton(text="", callback_data="my_subscriptions")
-profile_button3 = InlineKeyboardButton(text="💎 Мои подписки", callback_data="my_subscriptions")
